41f96fc524b8e9e5c4428c5bc78bc7d3e865ebc5
Armanda Caesario Cornelis <armanda.c.cornelis@banksinarmas.com>
2024-08-01 13:03:37 +0700

    add consumer draft

--- infrastructure/messaging/kafka/consumer.go
+++ infrastructure/messaging/kafka/consumer.go
@@ -1,6 +1,20 @@
 package kafka
 
+import "github.com/confluentinc/confluent-kafka-go/kafka"
+
 type ConsumerHandler func(err error)
 
 type Consumer struct {
+	Instance         kafka.Consumer
+	BootstrapServers string
+}
+
+func NewConsumer(bootstrapServer, groupID string) *Consumer {
+	_instance, _ := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": bootstrapServer,
+		"group.id":          groupID,
+		"auto.offset.reset": "earliest",
+	})
+
+	return _instance
 }
