1a1e15a1bd1e12f7ebf5f0a99bec584eaa82827f
Armanda Caesario Cornelis <armanda.c.cornelis@banksinarmas.com>
2024-08-01 16:38:40 +0700

    fix main

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -4,7 +4,7 @@ import "go-rmq/infrastructure/messaging/kafka"
 
 func main() {
 
-	consumer := kafka.NewConsumer("localhost:9092", "test-group")
+	consumer := kafka.NewConsumer("localhost:9092", "test-group", 100)
 
 	consumer.Consume(nil, "my-topic")
 }
