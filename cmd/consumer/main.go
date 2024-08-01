66b5fbdce576d79e2b259bb31c0eeae5d33990a3
Armanda Caesario Cornelis <armanda.c.cornelis@banksinarmas.com>
2024-08-01 16:25:48 +0700

    add consumer

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,3 +1,10 @@
 package main
 
-func main() {}
+import "go-rmq/infrastructure/messaging/kafka"
+
+func main() {
+
+	consumer := kafka.NewConsumer("localhost:9092", "test-group")
+
+	consumer.Consume(nil, "my-topic")
+}
--- infrastructure/messaging/kafka/consumer.go
+++ infrastructure/messaging/kafka/consumer.go
@@ -1,13 +1,21 @@
 package kafka
 
-import "github.com/confluentinc/confluent-kafka-go/kafka"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
 
 type ConsumerHandler func(err error)
 
 type Consumer struct {
-	Instance         kafka.Consumer
+	Instance         *kafka.Consumer
 	BootstrapServers string
 }
+
+type KafkaConsumerHandler func(event interface{}, err error)
 
 func NewConsumer(bootstrapServer, groupID string) *Consumer {
 	_instance, _ := kafka.NewConsumer(&kafka.ConfigMap{
@@ -16,5 +24,43 @@ func NewConsumer(bootstrapServer, groupID string) *Consumer {
 		"auto.offset.reset": "earliest",
 	})
 
-	return _instance
+	return &Consumer{Instance: _instance, BootstrapServers: bootstrapServer}
+}
+
+func (c *Consumer) Consume(handler KafkaConsumerHandler, topic string) (err error) {
+	c.Instance.Subscribe(topic, nil)
+	if err != nil {
+		println("Failed to subscribe to topic: %s\n", err)
+		return err
+	}
+
+	// Setup a channel to handle OS signals for graceful shutdown
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, os.Interrupt)
+
+	// Start consuming messages
+	run := true
+	for run == true {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Received signal %v: terminating\n", sig)
+			run = false
+		default:
+			// Poll for Kafka messages
+			ev := c.Instance.Poll(100)
+			if ev == nil {
+				continue
+			}
+
+			switch e := ev.(type) {
+			case *kafka.Message:
+				// Process the consumed message
+				fmt.Printf("Received message from topic %s: %s\n", *e.TopicPartition.Topic, string(e.Value))
+			case kafka.Error:
+				// Handle Kafka errors
+				fmt.Printf("Error: %v\n", e)
+			}
+		}
+	}
+	return nil
 }
